6aa433112578c5a5e9706cb9e37bb6f97b515a01
Jair Deon <jair.deon@gmail.com>
2022-08-25 23:53:09 -0300

    parte 13

---
+++ app/pt13.ts
@@ -0,0 +1,62 @@
+// noinspection DuplicatedCode
+
+enum MutationTypes {
+    setState = "_setState"
+}
+
+type Phone = {
+    phone: number;
+    responsible: string;
+    phone_type_id: number;
+}
+
+type PhoneState = {
+    phone: Phone | null
+}
+
+const state: PhoneState = {
+    phone: null
+}
+
+const mutations: MutationsType = {
+    [MutationTypes.setState](state, payload) {
+        state.phone && (state.phone[payload.propName] = payload.value);
+    }
+}
+
+type MutationsType<S = PhoneState> = {
+    _setState<PropName extends keyof Phone>(
+        state: S,
+        payload: PropNameKeyValue<PropName, Phone[PropName]>
+    ): void;
+}
+
+interface PropNameKeyValue<KeyType, ValueType> {
+    propName: KeyType;
+    value: ValueType;
+}
+
+const actions: Actions = {
+    setStateAction({commit}) {
+        commit(MutationTypes.setState, {propName: 'phone', value: 123456789});
+        commit(MutationTypes.setState, {propName: 'responsible', value: 'Jair Deon'});
+        commit(MutationTypes.setState, {propName: 'phone_type_id', value: 1});
+    }
+}
+
+interface Actions {
+    setStateAction(context: ActionsContextParam): void;
+}
+
+type ActionsContextParam = {
+    commit<Key extends keyof MutationsType>(
+        key: Key,
+        payload: AllowedValues
+    ): ReturnType<MutationsType[Key]>;
+};
+
+type Values<T> = T[keyof T];
+
+type AllowedValues = Values<{
+    [Prop in keyof Phone]: PropNameKeyValue<Prop, Phone[Prop]>
+}>;
