9a87abe8f983132aed7e32087914679c69c0703c
Jair Deon <jair.deon@gmail.com>
2022-08-25 23:11:19 -0300

    finalização da parte 10

--- app/pt10.ts
+++ app/pt10.ts
@@ -43,5 +43,20 @@ const actions: Actions = {
 }
 
 interface Actions {
-    setStateAction(context: any): void;
+    // A função setStateAction, possui um parâmetro com o nome de context
+    // o parâmetro context, espera receber um parâmetro com o nome de commit, que é uma das regras definidas no tipo ActionsContextParam
+    setStateAction(context: ActionsContextParam): void;
 }
+
+// O tipo ActionsContextParam, é um objeto que possui uma função chamada commit
+// A função commit, espera receber dois parâmetros:
+// // Uma chave, que deverá ser uma constante do tipo MutationTypes, que é uma enumeração dos tipos de mutações
+// // Um payload, que por enquanto aceitará qualquer tipo de dado
+// No final, a função commit retornará um ReturnType que é o tipo de retorno da função commit
+// O ReturnType retorna o tipo de retorno de um tipo de função, que receberá como chave o tipo da mutação informada no parâmetro da função commit
+type ActionsContextParam = {
+    commit<Key extends keyof MutationsType>(
+        key: Key,
+        payload: any
+    ): ReturnType<MutationsType[Key]>;
+};
