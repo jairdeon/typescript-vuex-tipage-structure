f6353f3c3061c3997bebf2c1084a2e0e2ba4a087
Jair Deon <jair.deon@gmail.com>
2022-08-25 22:35:42 -0300

    finalização da parte 7

--- app/pt7.ts
+++ app/pt7.ts
@@ -1,3 +1,10 @@
+// O enum MutationTypes, será utilizado para que ao chamar a constante mutations, seja possível atribuir de forma dinâmica
+// então utilizar [MutationTypes.setMetaProp] como nome da função, será o mesmo que chamar a função _setState
+// O tipo MutationType espera receber uma função com o nome _setState, e ao utilizar o enum MutationTypes.setMetaProp, será identificado como _setState, então será válido
+enum MutationTypes {
+    setMetaProp = "_setState"
+}
+
 type Phone = {
     phone: boolean;
     responsible: string;
@@ -13,13 +20,13 @@ const state: PhoneState = {
 }
 
 const mutations: MutationsType = {
-    _setState(state, payload) {
+    [MutationTypes.setMetaProp](state, payload) {
         state.phone && (state.phone[payload.propName] = payload.value);
     }
 }
 
 type MutationsType<S = PhoneState> = {
-    _setState <PropName extends keyof Phone>(
+    _setState<PropName extends keyof Phone>(
         state: S,
         payload: PropNameKeyValue<PropName, Phone[PropName]>
     ): void;
