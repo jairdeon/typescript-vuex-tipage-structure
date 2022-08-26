fba051366ae3d0a907ca55f27492aea7fabb8bfa
Jair Deon <jair.deon@gmail.com>
2022-08-25 23:58:52 -0300

    Index com comentários - Parte 3

--- indexWithComments.ts
+++ indexWithComments.ts
@@ -6,14 +6,29 @@ export const state: State = {
     test: true
 }
 
-// A função abaixo chamada setState, recebe 3 parâmetros
-// T é o objeto (que herdará o seu tipo (State)
-// K, a chave que precisará existir também entre os tipos de T (State)
-// value, que precisará ser um tipo compatível com o valor esperado de State[test] (boolean)
-function setState<T, K extends keyof T>(state: T, key: K, value: T[K]) {
-    return state[key] = value;
+// A função abaixo receberá 3 parâmetros, sendo eles o T e o K
+// T será o estado (State) desejado
+// K será a chave que será buscada no estado (State)
+// value será o valor que será atribuído a chave K
+function  _setState<T, K extends keyof T>(state: T, key: K, value: T[K]){
+    state[key] = value;
 }
 
-// Executando a função setState, passando a constante do tipo State como referência
-// buscando pelo campo 'test', com o valor true (boolean)
-setState(state, 'test', true);
+// A função abaixo, executará a _setState, passando o estado como referência
+// o campo 'test' como chave, e o valor true (boolean)
+// A chave 'test', deve ser uma chave existente no estado (State)
+// O valor true, deve ser compatível com o tipo do valor do campo 'test' (boolean)
+_setState(state, 'test', true);
+
+// A função abaixo receberá 3 parâmetros, sendo eles o T e o K
+// T será o estado (State) desejado
+// K será a chave que será buscada no estado (State)
+// value será o valor que será atribuído a chave K
+function actionState<T, K extends keyof T>(state: T, key: K, value: T[K]) {
+    // Esta ação executará a função _setState, que também aguardará 3 parâmetros do tipo T, K e value
+    _setState(state, key, value);
+}
+
+// A função abaixo executará a actionState, recebendo a constante state (do tipo State),
+// buscando o campo 'test' e setando o seu valor para true (boolean
+actionState(state, 'test', true);
