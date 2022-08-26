783e7bd75209c0fc440df1527bdf8131dce4de35
Jair Deon <jair.deon@gmail.com>
2022-08-25 23:59:16 -0300

    Index com comentários - Parte 5

--- indexWithComments.ts
+++ indexWithComments.ts
@@ -1,32 +1,39 @@
-export type State = {
-    test: boolean;
+// Agora, temos um novo tipo chamado Phone
+// O tipo phone, será um objeto
+export type Phone = {
+    phone: boolean;
+    responsible: string;
+    phone_type_id: number;
 }
 
-export const state: State = {
-    test: true
+// O tipo PhoneState, será um objeto que receberá em sua chave "phone"
+// um objeto do tipo Phone, ou será um objeto vazio
+type PhoneState = {
+    phone: Phone | null
 }
 
-// A constante abaixo, retornara o tipo "MutationsType"
-// O tipo "MutationsType" possui por padrão o tipo genérico setado como (State), sendo assim
-// a constante abaixo, já sabe que o seu estado é do tipo State
+// O estado por padrão, retornará o tipo PhoneState
+// e por padrão, o valor do campo phone é null
+const state: PhoneState = {
+    phone: null
+}
+
+// A constante mutations, retornará um objeto do tipo MitationsType
+// Este objeto, por padrão sabe que é do tipo PhoneState, então seu estado será do tipo PhoneState
+// Seu payload por enquanto não possui um tipo definido, então será do tipo any
 const mutations: MutationsType = {
-    // A função abaixo, será uma mutação chamada _setState, ela espera receber dois parâmetros:
-    // state, que será o estado (State) desejado
-    // payload, que por enquanto não possui um tipo definido
-    // Por causa do MutationsType estar definido como MutationsType<State>, o primeiro parâmetro state
-    // já assume que seus possíveis valores são do tipo State
+    // A função _setState, recebe um objeto do tipo PhoneState, e um payload que ainda não possui um tipo definido
     _setState(state, payload) {
-        state.test = payload.value;
+        // Caso o estado possua o campo phone, e dentro deste campo (objeto) exista o campo responsible,
+        // seu valor será o valor enviado pelo parâmetro payload
+        state.phone && (state.phone.responsible = payload.value);
     }
 }
 
-// O tipo MutationsType por padrão, possui um tipo genérico S setado como State, sendo assim
-// a constante abaixo, já sabe que o seu tipo genérico é do tipo State
-type MutationsType<S = State> = {
-    // A função abaixo, será uma mutação chamada _setState, ela espera receber dois parâmetros:
-    // state, que será o estado (State) desejado, e que por padrão estará utilizando o tipo genérico neste caso definido por padrão como State
-    // payload, que por enquanto não possui um tipo definido
-    // Esta função não retornará nenhum valor
+// O tipo MutationsType, por padrão recebe um tipo genérico do tipo PhoneState
+// O tipo MutationsType, será um objeto que possuirá uma função chamada _setState,
+// e essa função receberá um objeto do tipo PhoneState e um payload que ainda não possui um tipo definido
+type MutationsType<S = PhoneState> = {
     _setState(
         state: S,
         payload: any
